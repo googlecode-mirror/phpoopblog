7df516a2dafac58936e39d6662028ae0475563c2
muhammetable@gmail.com <muhammetable@gmail.com@90fc087b-5c16-52d3-3666-e8479d573ab3>
2012-03-24 15:39:33 +0000

    header file

---
+++ trunk/sablonlar/cek/ust.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="tr">
+    <head>
+    <title><?php echo htmlspecialchars($sonuclar['sayfaBasligi']) ?></title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+<div id="container">
+
+    
+
